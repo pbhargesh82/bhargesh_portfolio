aedf6eb5a335715d32f32111d4efeddddd673cce
Bhargesh Patel <pbhargesh82@aol.com>
2024-11-29 02:33:06 +0530

    hover_fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Social, socials } from './shared/constants/socials';
-import { techStacks, TechStack } from './shared/constants/stack';
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
+import { Social, socials } from '@constants/socials';
+import { techStacks, TechStack } from '@constants/stack';
 import { WorkExperience, workExperiences } from '@constants/work-exp';
 import { Project, projects } from '@constants/projects';
+import { angrySmiley, smileys } from '@constants/smileys';
 
 @Component({
   selector: 'app-root',
@@ -15,18 +16,48 @@ import { Project, projects } from '@constants/projects';
 })
 export class AppComponent {
 
-  title = 'bhargesh_portfolio';
+  title = 'Bhargesj Patel';
   apiUrl: string = 'https://v2.jokeapi.dev/joke/Programming?type=single';
   jokeData: any;
   techStacks: TechStack[] = techStacks;
   workExperiences: WorkExperience[] = workExperiences;
   projects: Project[] = projects;
   socials: Social[] = socials;
+  isHovered: boolean = false;
+  smileys: string[] = smileys;
+  angrySmiley: string = angrySmiley;
+  smileyIcon?: string;
+  currentIndex: number = 0;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getJokeData();
+    this.setUpDefaults();
+  }
+
+  setUpDefaults(): void {
+    this.smileyIcon = this.smileys[this.currentIndex];
+    setInterval(() => {
+      if (!this.isHovered) {
+        this.showNextIcon();
+      }
+    }, 2000);
+  }
+
+  showNextIcon(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.smileys.length;
+    this.smileyIcon = this.smileys[this.currentIndex];
+  }
+
+  onHover(state: boolean): void {
+    this.isHovered = state;
+    console.log(this.isHovered);
+    if (this.isHovered) {
+      this.smileyIcon = this.angrySmiley;
+    } else {
+      this.smileyIcon = this.smileys[this.currentIndex];
+    }
   }
 
   getJokeData(): any {
---
+++ src/app/shared/constants/smileys.ts
@@ -0,0 +1,14 @@
+export const smileys: string[] = [
+    '/assets/smileys/cheerful.svg',
+    '/assets/smileys/disapointed.svg',
+    '/assets/smileys/happy.svg',
+    '/assets/smileys/lol.svg',
+    // '/assets/smileys/mad.svg',
+    '/assets/smileys/prank.svg',
+    '/assets/smileys/sad.svg',
+    '/assets/smileys/smile.svg',
+    '/assets/smileys/unhappy.svg',
+    '/assets/smileys/wrong.svg',
+];
+
+export const angrySmiley: string = '/assets/smileys/mad.svg';
